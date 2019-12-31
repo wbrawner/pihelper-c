fb9ec1184abe08d839e8fba377298960fc1b1683
William Brawner <me@wbrawner.com>
2019-12-31 14:14:22 -0600

    Add GPL-3 license

--- PiHelper/cli.c
+++ PiHelper/cli.c
@@ -1,3 +1,21 @@
+/**
+ * Copyright © 2019, 2020 William Brawner.
+ *
+ * This file is part of PiHelper.
+ *
+ * PiHelper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PiHelper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PiHelper.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
